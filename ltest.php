a54cc4a05c8e97eb745dca055ac1ec6e325b52b6
richard <blueapple_he@qq.com>
2016-03-16 14:31:57 +0800

    test

--- ltest.php
+++ ltest.php
@@ -5,6 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 header('Content-type: text/html;charset=UTF-8');
 define("BASE_PATH", __DIR__);
 define("LANG", "zh_CN");
