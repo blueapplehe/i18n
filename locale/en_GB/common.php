7251229f8fcafd587cc6e32fc4564139cc193287
richard <blueapple_he@qq.com>
2016-03-16 16:58:46 +0800

    test

---
+++ locale/en_GB/common.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * 英文为源语言，可以不添加翻译
+ */
+return [
+	"hello"=>"hello",
+	"hello world!"=>"hello world!",
+	"{name},hello"=>"{name},hello",
+];
