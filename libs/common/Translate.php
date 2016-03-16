3a439f886a55cadb6a283dda401fcc283b953163
richard <blueapple_he@qq.com>
2016-03-16 13:34:28 +0800

    test

--- libs/common/Translate.php
+++ libs/common/Translate.php
@@ -6,12 +6,26 @@
  * and open the template in the editor.
  */
 require_once '/../language/I18N.php';
-
+/**
+ * 全局翻译方法
+ * @param type $message
+ * @param type $category
+ * @param type $params
+ * @param type $language
+ * @return type
+ */
 function t($message, $category = "*", $params = array(), $language = "")
 {
 	return I18N::getInstance()->translate($category, $message, $params, $language);
 }
-
+/**
+ * 全局翻译方法
+ * @param type $message
+ * @param type $category
+ * @param type $params
+ * @param type $language
+ * @return type
+ */
 function __($message, $category = "*", $params = array(), $language = "")
 {
 	return I18N::getInstance()->translate($category, $message, $params, $language);
---
+++ libs/language/DbMessageSource.php
@@ -0,0 +1,86 @@
+<?php
+require_once 'MessageSource.php';
+class DbMessageSource extends MessageSource
+{
+    const CACHE_KEY_PREFIX = 'DbMessageSource';
+
+    public $db = 'db';
+	
+    public $cache = 'cache';
+
+	
+    public $sourceMessageTable = 'source_message';
+
+    public $messageTable = 'message';
+
+	
+    public $cachingDuration = 0;
+
+    public $enableCaching = false;
+
+
+    public function init()
+    {
+        $this->db = Instance::ensure($this->db, Connection::className());
+        if ($this->enableCaching) {
+            $this->cache = Instance::ensure($this->cache, Cache::className());
+        }
+    }
+	
+	/**
+	 * 
+	 * @param type $category
+	 * @param type $language
+	 * @return type
+	 */
+    protected function loadMessages($category, $language)
+    {
+        if ($this->enableCaching) {
+            $key = [
+                __CLASS__,
+                $category,
+                $language,
+            ];
+            $messages = $this->cache->get($key);
+            if ($messages === false) {
+                $messages = $this->loadMessagesFromDb($category, $language);
+                $this->cache->set($key, $messages, $this->cachingDuration);
+            }
+
+            return $messages;
+        } else {
+            return $this->loadMessagesFromDb($category, $language);
+        }
+    }
+
+	/**
+	 * 从数据库中加载翻译数组
+	 * @param type $category
+	 * @param type $language
+	 * @return type
+	 */
+    protected function loadMessagesFromDb($category, $language)
+    {
+        $mainQuery = new Query();
+        $mainQuery->select(['t1.message message', 't2.translation translation'])
+            ->from(["$this->sourceMessageTable t1", "$this->messageTable t2"])
+            ->where('t1.id = t2.id AND t1.category = :category AND t2.language = :language')
+            ->params([':category' => $category, ':language' => $language]);
+
+        $fallbackLanguage = substr($language, 0, 2);
+        if ($fallbackLanguage != $language) {
+            $fallbackQuery = new Query();
+            $fallbackQuery->select(['t1.message message', 't2.translation translation'])
+                ->from(["$this->sourceMessageTable t1", "$this->messageTable t2"])
+                ->where('t1.id = t2.id AND t1.category = :category AND t2.language = :fallbackLanguage')
+                ->andWhere("t2.id NOT IN (SELECT id FROM $this->messageTable WHERE language = :language)")
+                ->params([':category' => $category, ':language' => $language, ':fallbackLanguage' => $fallbackLanguage]);
+
+            $mainQuery->union($fallbackQuery, true);
+        }
+
+        $messages = $mainQuery->createCommand($this->db)->queryAll();
+
+        return ArrayHelper::map($messages, 'message', 'translation');
+    }
+}
